2af65b1741f4f2eee73adc87013d719c14b6bde7
Renzo Mathias Nepomuceno <renzomathiasnepo@gmail.com>
2026-06-03 16:32:00 -0300

    Primeiro Cod de Java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Renzo";
+        int idade = 23;
+        double altura = 1.80;
+        System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos e " + altura + " de altura");
+    }
+}
