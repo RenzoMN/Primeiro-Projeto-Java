f78cc901d2ccaa634164fee6da4c12efef03def1
Renzo Mathias Nepomuceno <renzomathiasnepo@gmail.com>
2026-06-04 11:35:44 -0300

    Atribuindo variáveis e aprendendo funções como .format() e conversão de tipos (casting)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        String nome = "Renzo";
-        int idade = 23;
-        double altura = 1.80;
-        System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos e " + altura + " de altura");
+
+        Object string;
+        int anoDeLancamento = 2022;
+        double notaDoFilme = 8.1;
+        boolean incluidoNoPlano = true;
+        double media = (9.8 + 6.3 + 8.0) / 3;         //Média calculada atráves de 3 notas aleátorias
+        String sinopse;
+        sinopse = """
+                Top Gun: Maverick é a sequência de 2022 de Top Gun: Ases Indomáveis,
+                ambientada mais de 30 anos após o original.  A trama acompanha o Capitão Pete "Maverick" Mitchell (Tom Cruise),
+                um piloto de testes ousado e insubordinado que se recusa a avançar na carreira para permanecer voando.
+                """;
+
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println(media);
+        System.out.println(sinopse);
+
+        //Agora vou usar o .format para testar a usabilidade dessa função
+        System.out.println(String.format("O filme lançou no ano de %d sua nota é %.2f", anoDeLancamento, notaDoFilme));
+
+        //Testando identificador e classificando filme
+        int classificacao;
+        classificacao = (int) (media/2);
+        System.out.println(classificacao);  //existe uma tabela de Casting e as linhas 26 e 27 poderiam estar na mesma linha
     }
 }
