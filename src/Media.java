eb27d8aec7a7db3206422d3cf2fca92af8629517
Renzo Mathias Nepomuceno <renzomathiasnepo@gmail.com>
2026-06-11 14:44:47 -0300

    Casting e media

---
+++ src/Media.java
@@ -0,0 +1,9 @@
+public class Media {
+    public static void main(String[] args) {
+        double nota1 = 7.8;
+        int nota2 = 9;
+        int nota1Int = (int) nota1;
+        int media = (nota1Int + nota2) / 2;
+        System.out.println(media);
+    }
+}
